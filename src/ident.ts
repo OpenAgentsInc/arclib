c784644f7d45b6d0694c3b74933f58be10fd774f
erik aronesty <erik@q32.com>
2023-05-10 13:34:36 -0400

    basic ident module

---
+++ src/ident.ts
@@ -0,0 +1,65 @@
+import {
+  validateEvent,
+  verifySignature,
+  signEvent,
+  getEventHash,
+  getPublicKey,
+  nip19,
+} from 'nostr-tools';
+
+import { strict as assert } from 'assert';
+
+export interface NostrEvent {
+  id: string;
+  kind: number;
+  pubkey: string;
+  tags: string[];
+  content: string;
+  sig: string;
+  created_at: number;
+}
+
+export class ArcadeIdentity {
+  public privKey: string;
+  public pubKey: string;
+
+  constructor(
+    public nsec: string,
+    public bitcoinAddress: string,
+    public lnUrl: string
+  ) {
+    this.nsec = nsec;
+    const { type, data } = nip19.decode(nsec);
+    this.privKey = data;
+    assert(type == 'nsec');
+    this.bitcoinAddress = bitcoinAddress;
+    this.lnUrl = lnUrl;
+    this.pubKey = getPublicKey(this.privKey);
+  }
+
+  async signEvent(
+    event: Omit<NostrEvent, 'id' | 'sig' | 'created_at' | 'pubkey'>,
+    privateKey: string
+  ): Promise<NostrEvent> {
+    const { kind, tags, content } = event;
+    const created_at = Math.floor(Date.now() / 1000);
+    const ret: NostrEvent = {
+      kind: kind,
+      tags: tags,
+      content: content,
+      created_at: created_at,
+      id: '',
+      sig: '',
+      pubkey: '',
+    };
+    ret.pubkey = this.pubKey;
+    ret.id = getEventHash(ret);
+    ret.sig = signEvent(ret, this.privKey);
+    return ret;
+  }
+
+  isMine(event: NostrEvent): boolean {
+    const { pubkey } = event;
+    return pubkey == this.pubKey;
+  }
+}
--- src/listing.js
+++ src/listing.js
@@ -1,22 +1,9 @@
-type TradeCommand = {
-  action: 'BUY' | 'SELL' | 'buy' | 'sell';
-  minAmount?: number;
-  maxAmount: number;
-  currency: string;
-  price: number;
-  paymentTags: string;
-  expirationDays?: number;
-  additionalData?: Record<string, number | string>;
-};
-
 const knownAliases = {
   expiration: /expi?r?a?t?i?o?n?/,
 };
-
-function parseNameValuePairs(input: string): Record<string, string> {
+function parseNameValuePairs(input) {
   const pairs = input.split(' ');
-  const result: Record<string, string> = {};
-
+  const result = {};
   console.log('pairs', pairs);
   pairs.forEach((pair) => {
     const [originalName, value] = pair.split(':');
@@ -28,25 +15,19 @@ function parseNameValuePairs(input: string): Record<string, string> {
     });
     result[name.trim().toLowerCase()] = value.trim();
   });
-
   return result;
 }
-
-export function parseDecimalNumber(number: string) {
+export function parseDecimalNumber(number) {
   return parseFloat(number.replace(',', ''));
 }
-
-export function parseCommand(command: string): TradeCommand {
+export function parseCommand(command) {
   const regex =
     /^(B?U?Y?|S?E?L?L?)\s+([.\d,]+)-?([.\d,]+)\s*(\w+)\s*@\s*([\d,.]+)\s*\[(\w+)\]\s*(.*)?$/i;
   const matches = command.match(regex);
-
   console.log(matches);
-
   if (!matches) {
     throw new Error('Invalid command format');
   }
-
   // first res in array is whole match
   const [
     ,
@@ -58,29 +39,23 @@ export function parseCommand(command: string): TradeCommand {
     paymentTag,
     additional,
   ] = matches;
-
-  const result: TradeCommand = {
-    action: action.toUpperCase() as 'BUY' | 'SELL',
+  const result = {
+    action: action.toUpperCase(),
     minAmount: minAmount ? parseDecimalNumber(minAmount) : undefined,
     maxAmount: parseDecimalNumber(maxAmount),
     currency,
     price: parseDecimalNumber(price),
     paymentTags: paymentTag,
   };
-
   const addData = parseNameValuePairs(additional);
-
   if (addData.exp) {
     // todo: hours, minutes, whatever
     result.expirationDays = parseInt(addData.exp.replace('d', ''), 10);
     delete addData.exp;
   }
-
   result.additionalData = addData;
-
   return result;
 }
-
 export function createCommand({
   action,
   minAmount,
@@ -90,7 +65,7 @@ export function createCommand({
   price,
   expirationDays,
   additionalData,
-}: TradeCommand): string {
+}) {
   if (action.toLowerCase() !== 'buy' && action.toLowerCase() !== 'sell') {
     throw new Error("Invalid action. Action must be either 'buy' or 'sell'");
   }
@@ -102,7 +77,7 @@ export function createCommand({
     minAmount === maxAmount ? `${minAmount}` : `${minAmount}-${maxAmount}`;
   const tags = paymentTags ? `[${paymentTags.toUpperCase()}] ` : '';
   const expDays = expirationDays ? `EXP:${expirationDays}d` : '';
-  let addData = { ...additionalData };
+  let addData = Object.assign({}, additionalData);
   if (expirationDays) addData['exp'] = expirationDays.toString() + 'd';
   const additionalFields = Object.entries(addData)
     .filter(([key]) => key !== 'expirationDays')
