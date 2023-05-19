3aa30a8f28541d8e0ad7509c46eda2e39c4f0a14
erik aronesty <erik@q32.com>
2023-05-19 13:03:36 -0400

    add offers

--- src/listing.ts
+++ src/listing.ts
@@ -2,7 +2,7 @@ import Nip28Channel from "./channel";
 import { NostrEvent } from "./ident";
 
 interface ArcadeListingInput {
-  type: "v1";
+  type: "l1";
   action: "buy" | "sell";
   item: string;
   content?: string
@@ -15,17 +15,42 @@ interface ArcadeListingInput {
 }
 
 interface ArcadeListing {
-  type: "v1";
-  action: "buy" | "sell";
-  item: string;
+  type: "l1";
+  action: "buy" | "sell";   
+  item: string;             // bitcoin, or anything else
+  content: string           // friendly message
+  price: number;            //
+  currency: string;         // espected currency for trade
+  amt: number;              // max amount for sale
+  min_amt?: number;         // min amount will accept (if not present... same as max)
+  payments: string[];       // list of payment methods
+  expiration: number;       // expiraton seconds
+  id?: string;
+  created_at?: number;       // epoch create time
+  tags?: string[];
+}
+
+interface ArcadeOfferInput {
+  type: "o1";
+  listing_id: string;   // source listing id
+  content?: string      // nice message with any details
+  price: number;        // price offered (if different from listing)
+  currency?: string;    // currency offered (if different from listing)
+  amt: number;          // amount offered (should be >= min_amt <= amt)
+  payment: string;     // payment type selection
+  expiration: string;   // offer should be ignored after this time
+}
+
+interface ArcadeOffer {
+  type: "o1";
   content: string
   price: number;
   currency: string;
   amt: number;
-  min_amt?: number;
-  payments: string[];
+  payment: string;
   expiration: number;
   id?: string;
+  created_at?: number;       // epoch create time
   tags?: string[];
 }
 
@@ -48,12 +73,13 @@ export class ArcadeListings {
         const info: ArcadeListing = JSON.parse(tag[1])
         info.id = el.id
         info.content = el.content
+        info.created_at = el.created_at
         return info
     })
     return ents.filter((el)=>{return el != null}) as ArcadeListing[]
   }
 
-  async post(listing: ArcadeListingInput): Promise<NostrEvent> {
+  async post(listing: ArcadeListingInput): Promise<ArcadeListing> {
     const secs = convertToSeconds(listing.expiration)
     if (!secs) {
       throw new Error(`invalid expiration ${listing.expiration}`)
@@ -72,12 +98,51 @@ export class ArcadeListings {
     const tags = [["a", JSON.stringify(final)]]
     const content = listing.content ?? ""
     delete listing.content
-    return await this.conn.send(this.channel_id, content, undefined, tags)
+    const ev = await this.conn.send(this.channel_id, content, undefined, tags)
+    final.id = ev.id
+    final.created_at = ev.created_at
+    return final
   }
 
   async delete(listing_id: string): Promise<void> {
     // Implement the logic to delete a listing.
     // Use the provided listing_id to identify and remove the corresponding listing.
+  }
+
+  async postOffer(offer: ArcadeOfferInput): Promise<NostrEvent> {
+    const secs = convertToSeconds(offer.expiration)
+    if (!secs) {
+      throw new Error(`invalid expiration ${offer.expiration}`)
+    }
+    const final: ArcadeOffer = {
+      type: offer.type,
+      amt: offer.amt,
+      price: offer.price,
+      content: offer.content ? offer.content : "",
+      currency: offer.currency ? offer.currency : "",
+      expiration: secs,
+      payment: offer.payment
+    }
+    const tags = [["a", JSON.stringify(final)]]
+    const content = offer.content ?? ""
+    delete offer.content
+    return await this.conn.send(this.channel_id, content, offer.listing_id, tags)
+  }
+
+  async listOffers(listing_id: string): Promise<ArcadeOffer[]> {
+      const ents = (await this.conn.list(this.channel_id)).map((el: NostrEvent)=>{
+        const tag = el.tags.find((el)=>{return el[0] == "a"})
+        const repl = el.tags.find((el)=>{return el[0] == "e" && el[1] == listing_id})
+        if (!tag || !repl) {
+          return null
+        }
+        const info: ArcadeOffer = JSON.parse(tag[1])
+        info.id = el.id
+        info.content = el.content
+        info.created_at = el.created_at
+        return info
+    })
+    return ents.filter((el)=>{return el != null}) as ArcadeOffer[]
   }
 }
 
@@ -118,4 +183,34 @@ function convertToSeconds(input: string): number | undefined {
     default:
       return undefined; // Invalid unit
   }
+}
+
+
+export function formatDuration(duration: number): string {
+  if (duration < 0) {
+    throw new Error('Duration must be a non-negative number.');
+  }
+
+  const units: [string, number][] = [
+    ['d', 24 * 60 * 60],
+    ['h', 60 * 60],
+    ['m', 60],
+    ['s', 1],
+  ];
+
+  const parts: string[] = [];
+
+  for (const [unit, seconds] of units) {
+    const value = Math.floor(duration / seconds);
+    if (value > 0) {
+      parts.push(`${value}${unit}`);
+      duration -= value * seconds;
+    }
+  }
+
+  if (parts.length === 0) {
+    return '0s';
+  }
+
+  return parts.join(' ');
 }
